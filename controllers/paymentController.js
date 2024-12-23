9e5f7479c90c0156534d6a670d24dd69bfbfb0a6
Govind <choudharygovind553@gmail.com>
2024-12-24 01:16:03 +0530

    New commit

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,56 +1,35 @@
 const Razorpay = require('razorpay');
-const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
 
 const razorpayInstance = new Razorpay({
-    key_id: RAZORPAY_ID_KEY,
-    key_secret: RAZORPAY_SECRET_KEY
+    key_id: process.env.RAZORPAY_ID_KEY,
+    key_secret: process.env.RAZORPAY_SECRET_KEY,
 });
 
-const renderProductPage = async (req, res) => {
-    try {
-        res.render('product');
-    } catch (error) {
-        console.log(error.message);
-    }
+exports.getRazorpayKey = (req, res) => {
+    res.json({ key_id: process.env.RAZORPAY_ID_KEY });
 };
 
-const createOrder = async (req, res) => {
-    try {
-        const amount = req.body.amount * 100;
-        const options = {
-            amount: amount,
-            currency: 'INR',
-            receipt: 'razorUser@gmail.com'
-        };
+exports.initiatePayment = async (req, res) => {
+    const { amount, currency } = req.body;
+    const options = {
+        amount: amount * 100,
+        currency: currency,
+        receipt: `receipt_${Date.now()}`,
+    };
 
-        razorpayInstance.orders.create(options, (err, order) => {
-            if (!err) {
-                res.status(200).send({
-                    success: true,
-                    msg: 'Order Created',
-                    order_id: order.id,
-                    amount: amount,
-                    key_id: RAZORPAY_ID_KEY,
-                    product_name: 'Product',
-                    description: 'Product Description', 
-                    contact: req.body.phone,
-                    name: req.body.name,
-                    email: req.body.email,
-                    address: req.body.address,
-                    country: req.body.country,
-                    city: req.body.city
-                });
-            } else {
-                res.status(400).send({ success: false, msg: 'Something went wrong!' });
-            }
+    try {
+        const order = await razorpayInstance.orders.create(options);
+        res.json({
+            id: order.id,
+            amount: order.amount,
+            currency: order.currency,
         });
     } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ success: false, msg: 'Internal Server Error' });
+        console.error('Error creating Razorpay order:', error);
+        res.status(500).json({ error: 'Failed to initiate payment' });
     }
 };
 
-module.exports = {
-    renderProductPage,
-    createOrder
+exports.renderProductPage = (req, res) => {
+    res.render('product');
 };
--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const payment_route = express();
+const payment_route = express.Router();
 const bodyParser = require('body-parser');
+const paymentController = require('../controllers/paymentController');
+
 payment_route.use(bodyParser.json());
 payment_route.use(bodyParser.urlencoded({ extended: false }));
 
-const path = require('path');
-payment_route.set('view engine', 'ejs');
-payment_route.set('views', path.join(__dirname, '../views'));
-
-const paymentController = require('../controllers/paymentController');
-
 payment_route.get('/', (req, res) => {
-    res.render('product');
+    res.render('product'); 
 });
 
 payment_route.get('/product', paymentController.renderProductPage);
-
-// Route for creating order
-payment_route.post('/createOrder', paymentController.createOrder);
+payment_route.get('/get-razorpay-key', paymentController.getRazorpayKey);
+payment_route.post('/create-order', paymentController.initiatePayment);
 
 module.exports = payment_route;
